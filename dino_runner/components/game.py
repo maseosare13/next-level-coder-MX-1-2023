b5ef9e0c5a4f94129bb6efb4c9928f26c80de013
maseosare13 <Alexis.Ocejo@fundacion-jala.org>
2023-03-06 19:21:13 -0700

    Class 3: Obstacle

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -2,6 +2,7 @@ import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, SMALL_CACTUS
 from dino_runner.components.dinosaur import Dinosaur
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 
 class Game:
     def __init__(self):
@@ -18,7 +19,8 @@ class Game:
         self.y_pos_sc = 0
 
         self.player = Dinosaur()
-
+        self.obstacle_manager = ObstacleManager()
+    
     def run(self):
         # Game loop: events - update - draw
         self.playing = True
@@ -35,13 +37,15 @@ class Game:
 
     def update(self):
         user_input = pygame.key.get_pressed()
-        self.player.update(user_input   )
+        self.player.update(user_input)
+        self.obstacle_manager.update(self.game_speed, self)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()#actualiza los objetos
         pygame.display.flip()#muestra los cambios
 
